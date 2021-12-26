376f9a4697562421198f3523d45a4f6e826e659b
noradlb1 <zcyfkhuru.bz@mail.ru>
2021-12-26 07:28:32 +0100

    Duck-DNS-Updater-master
    Duck-DNS-Updater-master

---
+++ Duckdns/Form1.vb
@@ -0,0 +1,52 @@
+﻿
+
+
+Imports System
+Imports System.IO
+Imports System.Text
+Imports System.Threading
+Imports System.Net
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Task.Run(New Action(Sub()
+                                PostIP()
+                            End Sub))
+    End Sub
+
+    Private Sub PostIP()
+        Try
+            Dim x As HttpWebRequest = HttpWebRequest.CreateHttp(New Uri("https://duckdns.org/update/" & _
+            Me.TextBox1.Text & "/" & Me.TextBox2.Text & "/" & Me.TextBox3.Text))
+            x.GetResponse()
+            x.Abort()
+            MsgBox("Sucess!", MsgBoxStyle.DefaultButton1 + MsgBoxStyle.Information + vbSystemModal, "Sucess")
+        Catch ex As Exception
+            MsgBox("Erro!", MsgBoxStyle.DefaultButton1 + MsgBoxStyle.Critical + vbSystemModal, "Erro")
+        End Try
+    End Sub
+
+    Private Sub Form1_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
+        Try
+            Me.Dispose()
+            Me.Close()
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        MsgBox("Coded By MONSTERMC :)", MsgBoxStyle.DefaultButton1 + MsgBoxStyle.Information + vbSystemModal, ":)")
+
+        Try
+            Me.Dispose()
+            Me.Close()
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Control.CheckForIllegalCrossThreadCalls = False
+        Me.Text = "Duckdns Updeter V" & My.Application.Info.Version.ToString
+    End Sub
+End Class
---
+++ Duckdns/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Duckdns")> 
+<Assembly: AssemblyDescription("Duckdns")> 
+<Assembly: AssemblyCompany("Pjoao1578")> 
+<Assembly: AssemblyProduct("Duckdns")> 
+<Assembly: AssemblyCopyright("Copyright ©  2017")> 
+<Assembly: AssemblyTrademark("Duckdns")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1593250b-aa8b-4736-be91-f55a946a993e")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
